cebb1eefe0012a2d0b8778de1637e6f5d6db33ba
Baovola <baovolarav@gmail.com>
2023-10-03 18:14:10 +0200

    ajustement navigation en SPA avec UI

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -3,42 +3,46 @@ import Box from "@mui/material/Box";
 import Link from "@mui/material/Link";
 import HomeIcon from "@mui/icons-material/Home";
 import WhatshotIcon from "@mui/icons-material/Whatshot";
+//import AppBar from "@mui/material/AppBar";
+import { Link as RouterLink } from "react-router-dom";
 
 const Navigation = () => {
   return (
-    <div>
-      <Box
-        sx={{
-          display: "flex",
-          flexWrap: "wrap",
-          justifyContent: "center",
-          typography: "body1",
-          "& > :not(style) ~ :not(style)": {
-            ml: 4,
-          },
-        }}
+    // <AppBar sx={{ backgroundColor: "dark", height: "4vh", width: "100%" }}>
+    <Box
+      sx={{
+        display: "flex",
+        flexWrap: "wrap",
+        justifyContent: "center",
+        typography: "body1",
+        "& > :not(style) ~ :not(style)": {
+          ml: 4,
+        },
+      }}
+    >
+      <Link
+        component={RouterLink}
+        to="/"
+        underline="hover"
+        sx={{ display: "flex", alignItems: "center" }}
+        color="inherit"
       >
-        <Link
-          underline="hover"
-          sx={{ display: "flex", alignItems: "center" }}
-          color="inherit"
-          href="/"
-        >
-          <HomeIcon sx={{ mr: 0.5 }} fontSize="inherit" />
-          Home
-        </Link>
+        <HomeIcon sx={{ mr: 0.5 }} fontSize="inherit" />
+        Home
+      </Link>
 
-        <Link
-          underline="hover"
-          sx={{ display: "flex", alignItems: "center" }}
-          color="inherit"
-          href="/about"
-        >
-          <WhatshotIcon sx={{ mr: 0.5 }} fontSize="inherit" />
-          About
-        </Link>
-      </Box>
-    </div>
+      <Link
+        component={RouterLink}
+        to="/about"
+        underline="hover"
+        sx={{ display: "flex", alignItems: "center" }}
+        color="inherit"
+      >
+        <WhatshotIcon sx={{ mr: 0.5 }} fontSize="inherit" />
+        About
+      </Link>
+    </Box>
+    //  </AppBar>
   );
 };
 
